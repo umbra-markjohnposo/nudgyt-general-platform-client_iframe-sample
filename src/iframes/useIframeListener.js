5caea8f7237f0a63590d2a659a6acae361fc75be
Mark John Poso <markjohnposo@umbradigitalcompany.com>
2025-03-10 09:49:10 +0800

    feat(origins): allow all

--- src/iframes/useIframeListener.js
+++ src/iframes/useIframeListener.js
@@ -1,18 +1,8 @@
 import { useEffect } from "react";
-import originsConfig from "../config/origins";
 
 function useIframeListener(callback) {
   useEffect(() => {
     function receiveIframeMessage(event) {
-      const isEventOriginAllowed = originsConfig.ALLOWED_ORIGINS.includes(
-        event.origin
-      );
-      if (!isEventOriginAllowed) {
-        alert(`Origin not allowed: ${event.origin}`);
-
-        return;
-      }
-
       const messageData = JSON.parse(event.data);
 
       callback?.(messageData, event.origin);
